502ed608ec1bf79fe5e6132356dea32ac09ebaee
shshahbazi <shsh6184@gmail.com>
2024-08-02 22:41:15 +0330

    fix SECRET KEY env error

--- core/settings.py
+++ core/settings.py
@@ -9,17 +9,18 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os.path
 from pathlib import Path
+
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Environment variables settings
 import environ
 
 env = environ.Env()
-environ.Env.read_env()
-
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 
 # Quick-start development settings - unsuitable for production
@@ -55,7 +56,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'DHT_Backend.urls'
+ROOT_URLCONF = 'core.urls'
 
 TEMPLATES = [
     {
