fe8325270792b5c71ac774ea8fdf168d0b517d7b
shshahbazi <shsh6184@gmail.com>
2024-08-07 19:44:55 +0330

    add celery to the project

--- core/settings.py
+++ core/settings.py
@@ -179,3 +179,6 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 
+
+CELERY_BROKER_URL = 'redis://localhost:6379/0'
+
