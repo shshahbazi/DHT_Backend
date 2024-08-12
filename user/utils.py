a5d8d8c278381f6a5bff48f4e5fd343ed99fa483
shshahbazi <shsh6184@gmail.com>
2024-08-12 14:33:36 +0330

    add html template for sending otp

--- user/utils.py
+++ user/utils.py
@@ -1,11 +1,13 @@
 from django.core import exceptions
 from django.core.mail import EmailMessage
+from django.template.loader import render_to_string
 
 
 def send_otp(email, otp):
     try:
-        email_body = f'سلام دوست عزیز \nاز کد زیر برای ورود به دوست استفاده کن\n{otp}'
+        email_body = render_to_string('otp_email_template.html', {'otp_code': otp})
         email = EmailMessage(subject='ورود به دوست', body=email_body, to=[email])
+        email.content_subtype = 'html'
         email.send()
     except Exception as e:
         raise exceptions.BadRequest(f'An error occurred while sending email: {e}')
