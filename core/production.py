a0055ef90d0e0ae330e88321533376b6ada704c8
shshahbazi <shsh6184@gmail.com>
2024-08-19 14:41:01 +0330

    update production setting

--- core/production.py
+++ core/production.py
@@ -14,7 +14,7 @@ DATABASES = {
         "NAME": env('POSTGRES_DB'),
         "USER": env('POSTGRES_USER'),
         "PASSWORD": env('POSTGRES_PASSWORD'),
-        "HOST": 'db',
+        "HOST": 'dhtdb',
         "PORT": '5432',
     }
 }
