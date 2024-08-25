3f97103cc8bb7b34bf42dab110ad1242ae97949b
shshahbazi <shsh6184@gmail.com>
2024-08-25 21:00:46 +0330

    update model for mood tracker

--- mental/models.py
+++ mental/models.py
@@ -1,6 +1,9 @@
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
+from user.models import CustomUser
+
 
 class DailyMood(models.Model):
     mood = models.IntegerField(validators=[MaxValueValidator(5), MinValueValidator(1)])
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='daily_moods')
