08b6d2b54e481c29aee15ac997600d5536aced24
shshahbazi <shsh6184@gmail.com>
2024-08-29 00:44:42 +0330

    add model for daily quotes

--- mental/models.py
+++ mental/models.py
@@ -8,3 +8,14 @@ from user.models import CustomUser
 class DailyMood(BaseModel):
     mood = models.IntegerField(validators=[MaxValueValidator(5), MinValueValidator(1)])
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='daily_moods')
+
+
+class Quote(models.Model):
+    sentence = models.TextField()
+    mood = models.ForeignKey(DailyMood, on_delete=models.CASCADE)
+
+
+class DailyUserQuote(BaseModel):
+    sentence = models.TextField()
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='daily_quotes')
+
