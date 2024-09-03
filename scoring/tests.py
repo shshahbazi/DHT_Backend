c7c872c737c653e2a137383cae03a0157c0e6581
shshahbazi <shsh6184@gmail.com>
2024-09-03 21:58:26 +0330

    add tests for user score apis

--- scoring/tests.py
+++ scoring/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+from scoring.models import UserScore
+from user.models import CustomUser
+
+
+class UserScoreTests(APITestCase):
+
+    def setUp(self):
+        self.user = CustomUser.objects.create_user(email='test@example.com', password='password123')
+        self.user_score = UserScore.objects.get(user=self.user)
+        self.user_score.score = 100
+        self.user_score.save()
+        self.client.force_authenticate(user=self.user)
+
+    def test_add_points(self):
+        self.user_score.add_points(50)
+        self.assertEqual(self.user_score.score, 150)
+
+    def test_sub_points(self):
+        self.user_score.sub_points(50)
+        self.assertEqual(self.user_score.score, 50)
+
+    def test_sub_points_below_zero(self):
+        self.user_score.sub_points(150)
+        self.assertEqual(self.user_score.score, 0)
