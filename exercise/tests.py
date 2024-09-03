ac25c0dfab832909d1ce8c3fa985a0bc945085ac
shshahbazi <shsh6184@gmail.com>
2024-09-03 22:16:53 +0330

    add tests for exercise lists apis

--- exercise/tests.py
+++ exercise/tests.py
@@ -20,4 +20,17 @@ class ExerciseDetailViewTests(APITestCase):
         self.assertEqual(response.data['description'], self.exercise.description)
 
 
+class ExerciseListViewTests(APITestCase):
 
+    def setUp(self):
+        Exercise.objects.create(name="Exercise 1", description="This is the first test exercise.")
+        Exercise.objects.create(name="Exercise 2", description="This is the second test exercise.")
+
+    def test_get_exercise_list(self):
+        url = reverse('list_exercise')
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 2)
+        self.assertEqual(response.data[0]['name'], "Exercise 1")
+        self.assertEqual(response.data[1]['name'], "Exercise 2")
