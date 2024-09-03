e0355317f850c5294d9eb492c42dba89c7b44185
shshahbazi <shsh6184@gmail.com>
2024-09-03 21:38:06 +0330

    add tests for work session apis

--- habit/tests.py
+++ habit/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.utils import timezone
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+from habit.models import WorkSession
+from user.models import CustomUser
+
+
+class WorkSessionTests(APITestCase):
+
+    def setUp(self):
+        self.user = CustomUser.objects.create_user(email='test@example.com', password='password123')
+        self.client.force_authenticate(user=self.user)
+
+    def test_start_work_session(self):
+        response = self.client.get(reverse('start_work'))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertTrue(WorkSession.objects.filter(user=self.user, end_time=None).exists())
+
+    def test_end_work_session(self):
+        work_session = WorkSession.objects.create(user=self.user, start_time=timezone.now())
+        response = self.client.get(reverse('end_work'))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        work_session.refresh_from_db()
+        self.assertIsNotNone(work_session.end_time)
